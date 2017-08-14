745df01cd206b910c75c4411caedd7c7bf9f00d8
prey169 <30991896+prey169@users.noreply.github.com>
2017-08-13 21:52:09 -0400

    Added a logging function script
    logger.py is added

---
+++ logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+import re
+
+os.chdir("/Users/frossi/Python/test")
+
+files = os.listdir(".")
+regex = "^a.?\d*$"
+goodfiles = []
+numbers = []
+
+for i in files:
+    if re.match(regex, i) != None:
+        goodfiles.append(i)
+
+for i in goodfiles:
+    if "." in i:
+        numbers.append(int(i.strip("a.")))
+
+numbers=sorted(numbers, reverse=True)
+
+for i in numbers:
+    os.rename("a." + str(i), "a." + str(i+1))
+
+if 'a' in files:
+    os.rename("a", "a.1") 
+
